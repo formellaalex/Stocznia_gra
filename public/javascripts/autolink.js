c0847dcf35b3b94a3cde378a2f9e36a8d85befc7
formellaalex <formella.alex@gmail.com>
2015-12-21 00:18:07 +0100

    Poprawione linki

--- public/javascripts/autolink.js
+++ public/javascripts/autolink.js
@@ -10,10 +10,7 @@
     pattern = /(^|[\s\n]|<br\/?>)((?:https?|ftp):\/\/[\-A-Z0-9+\u0026\u2019@#\/%?=()~_|!:,.;]*[\-A-Z0-9+\u0026@#\/%=~()_|])/gi;
     if (!(options.length > 0)) {
       var link = this.replace(pattern, "$2");
-      if(link.length > 40){
-        return this.replace(pattern, "$1<a class='tresc_postu tytul black link' href='$2'>" + link.substring(0,40) + "(...)" +"</a>");
-      }
-      return this.replace(pattern, "$1<a class='tresc_postu tytul black link' href='$2'>$2</a>");
+        return this.replace(pattern, "$1<a class='tresc_postu tytul dark_red link' href='$2' target='_blank'>" + "LINK" +"</a>");
     }
     option = options[0];
     linkAttributes = ((function() {
@@ -29,7 +26,7 @@
     })()).join('');
     return this.replace(pattern, function(match, space, url) {
       var link;
-      link = (typeof option.callback === "function" ? option.callback(url) : void 0) || ("<a class='tresc_postu tytul black link' href='" + url + "'" + linkAttributes + ">" + "link" + "</a>");
+      link = (typeof option.callback === "function" ? option.callback(url) : void 0) || ("<a class='tresc_postu tytul dark_red link' target='_blank' href='" + url + "'" + linkAttributes + ">" + "LINK" + "</a>");
       return "" + space + link;
     });
   };
